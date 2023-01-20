b77e3091f5cd1959784f502cfab71cfbea3fa7f5
SirRamzi <aleks.aleksp@gmail.com>
2023-01-20 20:38:34 +0400

    Add Checkered_Boad class including the solution of task 1

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int result = Checkered_Board.constellations(12);
+        System.out.println(result);
+    }
+}
